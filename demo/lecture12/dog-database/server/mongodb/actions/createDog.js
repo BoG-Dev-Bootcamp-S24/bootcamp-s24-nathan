e9f5a85f968910e6fc5c6f463368555e59d13b4a
Nathan <nathandong68@gmail.com>
2024-03-13 00:16:14 -0400

    demo 12

---
+++ demo/lecture12/dog-database/server/mongodb/actions/createDog.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import connectDB from ".."
+import Dog from "../models/Dog"
+
+export default async function createDog(data) {
+    try {
+        await connectDB();
+        const dog = new Dog(data);
+        await dog.save();
+    
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+}
